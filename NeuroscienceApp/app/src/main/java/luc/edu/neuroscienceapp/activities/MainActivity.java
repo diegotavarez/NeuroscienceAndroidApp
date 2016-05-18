5bc116a3b5dd94f399e07e7800cb2636a654c0f3
diegotavarez <diegotavarez@github.com>
2016-05-18 17:20:30 -0500

    Showing picture after selecting from the library

--- NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/activities/MainActivity.java
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import luc.edu.neuroscienceapp.R;
 
@@ -22,6 +24,7 @@ public class MainActivity extends Activity {
     public static final int GALLERY_REQUEST = 2;
     public boolean imageSelected = false;
     private ImageButton btSelectPicture;
+    private Button btStart;
     private String selectedImagePath;
 
     @Override
@@ -36,6 +39,8 @@ public class MainActivity extends Activity {
                 selectImage();
             }
         });
+
+        btStart = (Button)findViewById(R.id.bt_start);
     }
 
     @Override
@@ -97,10 +102,18 @@ public class MainActivity extends Activity {
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
+
             if (requestCode == GALLERY_REQUEST) {
                 Uri selectedImageUri = data.getData();
                 selectedImagePath = getPath(selectedImageUri);
-                //btSelectPicture.setImageResource(selectedImagePath);
+                btSelectPicture.setImageURI(selectedImageUri);
+                btStart.setBackgroundColor(Color.parseColor("#00E676"));
+            }
+            else if (requestCode == CAMERA_REQUEST) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                btSelectPicture.setImageURI(selectedImageUri);
+                btStart.setBackgroundColor(Color.parseColor("#00E676"));
             }
         }
     }
