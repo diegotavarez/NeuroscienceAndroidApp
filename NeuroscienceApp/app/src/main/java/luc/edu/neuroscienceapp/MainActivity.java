8386331f197fcc31ebd9546aa961e44116895ac1
diegotavarez <diegotavarez@github.com>
2016-05-18 14:55:12 -0500

    Initial Screen of the app

--- NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/MainActivity.java
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/MainActivity.java
@@ -1,16 +1,36 @@
 package luc.edu.neuroscienceapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+    public static final int CAMERA_REQUEST = 1;
+    public static final int GALLERY_REQUEST = 2;
+    public boolean imageSelected = false;
+    public ImageButton btSelectPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btSelectPicture = (ImageButton)findViewById(R.id.select_picture);
+        btSelectPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
     }
 
     @Override
@@ -33,5 +53,40 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void selectImage(){
+        final String[] itemArray = getResources().getStringArray(R.array.dialog_options);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialog_title);
+        builder.setItems(itemArray, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (itemArray[which]){
+                    case "Take Photo":
+                    {
+                        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(takePhotoIntent, CAMERA_REQUEST);
+
+                        imageSelected = true;
+                        break;
+                    }
+
+                    case "Choose from Library":
+                    {
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        galleryIntent.setType("image/*");
+                        startActivityForResult(galleryIntent.createChooser(galleryIntent, "Select File"), GALLERY_REQUEST);
+
+                        imageSelected = true;
+                        break;
+                    }
+                    case "Cancel":
+                        dialog.cancel();
+                }
+            }
+        });
+        builder.show();
     }
 }
