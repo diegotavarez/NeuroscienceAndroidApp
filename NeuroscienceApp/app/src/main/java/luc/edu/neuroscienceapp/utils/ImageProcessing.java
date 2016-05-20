d79543f09fafb53b6c36a73b7941b2ecd1603b96
Laryssa Seabra <llaryssa@me.com>
2016-05-20 09:38:36 -0500

    adding gabor filter and PCA, making changes in ImageProcessing

--- NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/utils/ImageProcessing.java
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/utils/ImageProcessing.java
@@ -6,11 +6,12 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 
+import org.ejml.data.DenseMatrix64F;
+
 /**
  * Created by diegotavarez on 5/19/16.
  */
 public class ImageProcessing {
-
 
     public Bitmap toGrayscale(Bitmap bmpOriginal)
     {
@@ -27,6 +28,26 @@ public class ImageProcessing {
         paint.setColorFilter(f);
         c.drawBitmap(bmpOriginal, 0, 0, paint);
         return bmpGrayscale;
+    }
+
+    public DenseMatrix64F bitmap2Matrix(Bitmap bmp)
+    {
+        int width, height;
+        height = bmp.getHeight();
+        width = bmp.getWidth();
+        DenseMatrix64F matrix = new DenseMatrix64F(width, height);
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                matrix.add(i,j,bmp.getPixel(i,j));
+            }
+        }
+        return matrix;
+    }
+
+
+    public void process (Bitmap bmp) {
+        DenseMatrix64F img = bitmap2Matrix(bmp);
+
     }
 
 }
