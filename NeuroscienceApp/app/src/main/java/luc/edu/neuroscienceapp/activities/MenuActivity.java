7e55ec5e7bf1bc8ed65c26e204f5435171a2890f
Diego Tavares <diegotavarez@Diegos-MacBook-Pro.local>
2016-05-23 23:18:34 -0500

    SelectPictureActivity Removed

--- NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/activities/MenuActivity.java
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/activities/MenuActivity.java
@@ -1,8 +1,17 @@
 package luc.edu.neuroscienceapp.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,10 +19,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.io.IOException;
+
 import luc.edu.neuroscienceapp.R;
+import luc.edu.neuroscienceapp.entities.Global;
+import luc.edu.neuroscienceapp.imageprocessing.FileManagement;
 
 public class MenuActivity extends Activity {
-    ImageButton btExamples, btChoose;
+    public static final int CAMERA_REQUEST = 1;
+    public static final int GALLERY_REQUEST = 2;
+    public boolean imageSelected = false;
+    private String selectedImagePath;
+    private Bitmap imageBitmap;
+    private ProgressDialog mProgressDialog;
+
+    public String photoFileName = "photo.jpg";
+    public final String APP_TAG = "NeuroscienceApp";
+    ImageButton btExamples, btChoose, btGallery;
+    FileManagement fileManagement = new FileManagement();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +46,15 @@ public class MenuActivity extends Activity {
 
         btExamples = (ImageButton) findViewById(R.id.bt_examples);
         btChoose = (ImageButton) findViewById(R.id.bt_choose);
+        btGallery = (ImageButton) findViewById(R.id.bt_gallery);
 
         btChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this,SelectPictureActivity.class);
-                startActivity(intent);
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileManagement.getPhotoFileUri(photoFileName,getApplicationContext(),APP_TAG));
+                startActivityForResult(takePhotoIntent, CAMERA_REQUEST);
+                imageSelected = true;
             }
         });
 
@@ -39,6 +65,55 @@ public class MenuActivity extends Activity {
                 startActivity(intentGallery);
             }
         });
+
+        btGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent.createChooser(galleryIntent, "Select File"), GALLERY_REQUEST);
+                imageSelected = true;
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == GALLERY_REQUEST) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = fileManagement.getPath(selectedImageUri,this);
+                //btStart.setBackgroundColor(Color.parseColor("#00E676"));
+
+                try{
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                    imageBitmap = bitmap;
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (requestCode == CAMERA_REQUEST) {
+                Uri takenPhotoUri = fileManagement.getPhotoFileUri(photoFileName,getApplicationContext(),APP_TAG);
+                Bitmap bitmap = null;
+                try{
+                    bitmap = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                    imageBitmap = bitmap;
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                imageBitmap = bitmap;
+
+            }
+            mProgressDialog = new ProgressDialog(MenuActivity.this);
+            new ImageLoader().execute();
+            //Toast.makeText(getApplicationContext(),imageBitmap.getHeight() + " / " + imageBitmap.getWidth(),Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -61,5 +136,40 @@ public class MenuActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ImageLoader extends AsyncTask<Context, Integer, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create ProgressBar
+            //mProgressDialog = new ProgressDialog(getBaseContext());
+            // Set your ProgressBar Title
+            mProgressDialog.setTitle("Loading");
+            mProgressDialog.setIcon(R.drawable.camera_icon);
+            // Set your ProgressBar Message
+            mProgressDialog.setMessage("Converting image to grayscale channel");
+            //  mProgressDialog.setCanceledOnTouchOutside(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(Context... arg0) {
+
+            return imageBitmap;
+        }
+
+        @Override
+        protected void onPostExecute( Bitmap result )  {
+            Intent intent = new Intent(getApplicationContext(), ImageChannelConversionActivity.class);
+            //intent.putExtra("initial_image",result);
+
+            Global.img = result;
+            startActivity(intent);
+            mProgressDialog.dismiss();
+
+        }
+
     }
 }
--- NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/imageprocessing/FileManagement.java
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/imageprocessing/FileManagement.java
@@ -1,6 +1,12 @@
 package luc.edu.neuroscienceapp.imageprocessing;
 
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import java.io.File;
 
@@ -19,5 +25,49 @@ public class FileManagement {
         return File.createTempFile(part, ext, tempDir);
     }
 
+    public Uri getPhotoFileUri(String fileName, Context context, String APP_TAG) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(
+                    context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
 
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    // Returns true if external storage for photos is available
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
+    }
+
+    public String getPath(Uri uri, Activity activity) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = activity.managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
+    }
 }
