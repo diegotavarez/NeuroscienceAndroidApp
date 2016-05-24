8e452a6a56de9512165f513f9dd3e288b6ab3f94
Laryssa Seabra <llaryssa@me.com>
2016-05-24 11:29:04 -0500

    finally fixing PCA, gabor filter was outdated

--- NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/imageprocessing/GaborFilter.java
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/imageprocessing/GaborFilter.java
@@ -5,24 +5,42 @@ import org.ejml.simple.SimpleMatrix;
 /**
  * Created by llaryssa on 5/19/16.
  */
+
 public class GaborFilter {
     public SimpleMatrix filter;
+    public double mean;
 
-    public GaborFilter(int patchsize, double sx, double sy, double fx, double fy) {
-        this.filter = new SimpleMatrix(patchsize, patchsize);
+    public GaborFilter(int size, double sx, double sy, double fx, double fy) {
+        this.filter = new SimpleMatrix(size, size);
+        double accumulator = 0;
+        double pos_sum = 0;
+        double neg_sum = 0;
+
         double p1 = (1/(2*Math.PI*sx*sy));
-        for (int x = 0; x < patchsize; ++x) {
-            for (int y = 0; y < patchsize; ++y) {
-                double xx = x - ((double)(patchsize-1))/2;
-                double yy = y - ((double)(patchsize-1))/2;
+        for (int x = 0; x < size; ++x) {
+            for (int y = 0; y < size; ++y) {
+                double xx = x - ((double)(size-1))/2;
+                double yy = y - ((double)(size-1))/2;
                 double p2 = Math.exp(-0.5*(((xx*xx)/(sx*sx))+((yy*yy)/(sy*sy))));
                 double p3 = Math.cos(2*Math.PI*(fx*xx+fy*yy));
                 double value = p1*p2*p3;
                 this.filter.set(y, x, value);
+
+                if (value >= 0) pos_sum += value;
+                else neg_sum += value;
+                accumulator += value;
             }
         }
+        this.mean = accumulator/(size*size);
 
+        for (int x = 0; x < size; ++x) {
+            for (int y = 0; y < size; ++y) {
+                double value = this.filter.get(y, x);
+                if (value < 0) {
+                    this.filter.set(y, x, -pos_sum/neg_sum*value);
+                }
+            }
+        }
     }
 
 }
-
