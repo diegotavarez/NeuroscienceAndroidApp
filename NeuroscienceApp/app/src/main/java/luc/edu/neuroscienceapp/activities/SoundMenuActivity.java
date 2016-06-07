666944c059b1451cc7383a4f27c2478d5ac8f65b
annabreganholi <annabreganholi@github.com>
2016-06-07 11:22:00 -0500

    Main Sound

---
+++ NeuroscienceApp/app/src/main/java/luc/edu/neuroscienceapp/activities/SoundMenuActivity.java
@@ -0,0 +1,14 @@
+package luc.edu.neuroscienceapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import luc.edu.neuroscienceapp.R;
+
+public class SoundMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_menu);
+    }
+}
